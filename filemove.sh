e92b2bedf8371101f285489a0978c91e87ac8259
ji7777 <jishil7777@gmail.com>
2023-03-01 20:24:03 +0530

    dkdfjdifsgdhsdssdssdssddfsdssdsdsds

--- filemove.sh
+++ filemove.sh
@@ -5,11 +5,12 @@ find "$path" > f.log
 #storing user input file i.e; filename or substring
 x=$1
 c=0
-grep "$x" "f.log" | while read -r line; do
+grep "$x" "f.log" > grp.log
+while read -r line; do
 #copying file to target path..here taget path is python directory
 cp "$line" "/d/python"
-((c+=line))
-done
+((c++))
+done < grp.log
 echo $c
 if [ $c==0 ];then
 echo "no file found"
