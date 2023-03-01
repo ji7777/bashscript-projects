c4debddc7967908dde5779f294cd276ee4b5eb70
ji7777 <jishil7777@gmail.com>
2023-03-01 20:30:25 +0530

    More efficient script to search files in a directory and its sub directories , and to move those files.

--- filemove.sh
+++ filemove.sh
@@ -6,7 +6,7 @@ find "$path" > f.log
 #storing user input file i.e; filename or substring
 x=$1
 c=0
-grep "$x" "f.log" > grp.log
+grep -i "$x" "f.log" > grp.log
 while read -r line; do
 #copying file to target path..here taget path is python directory
 cp "$line" "$dest"
