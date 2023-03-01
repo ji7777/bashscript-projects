9c0f38068f7c892d19b7c4cbe75740a3d61b4cff
ji7777 <jishil7777@gmail.com>
2023-03-01 20:29:11 +0530

    More efficient script to search files in a directory and its sub directories , and to move those files.

--- filemove.sh
+++ filemove.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #storing the main directory where we are searching for files-- note tht below script will check in all child directories also
 path="/d"
+dest="/d/python"
 find "$path" > f.log
 #storing user input file i.e; filename or substring
 x=$1
@@ -8,7 +9,7 @@ c=0
 grep "$x" "f.log" > grp.log
 while read -r line; do
 #copying file to target path..here taget path is python directory
-cp "$line" "/d/python"
+cp "$line" "$dest"
 ((c++))
 done < grp.log
 echo $c
